8dba1b6c54a5b6e9e1698aa9a22884d0b27c142b
Edwin <edwin06111998@gmail.com>
2022-09-29 13:02:08 -0500

    Prueba

--- FRONT_VELOZ_EDWIN/main.py
+++ FRONT_VELOZ_EDWIN/main.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("requerimientos.csv", index_col="nombreE")
-
+#df = pd.read_csv("requerimientos.csv", index_col="nombreE")
+#df = pd.read_csv("etiquetas.csv", index_col="nombreE")
+df = pd.read_csv("categorias.csv", index_col="nombreE")
+nuevoDF = df.head(15)
 df.plot(kind = 'barh')
+#df.plot(kind = 'barh')
 plt.show()
